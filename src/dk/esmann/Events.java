058184dd656329b624a992499ca959303715d53e
Alex Esmann <alex-github@alexesmann.dk>
2013-06-25 23:42:16 +0200

    Now listing events, although recurrent events are only listed once.

--- src/dk/esmann/Events.java
+++ src/dk/esmann/Events.java
@@ -1,10 +1,16 @@
 package dk.esmann;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,6 +22,14 @@ import android.widget.Toast;
 public class Events extends Activity {
 
     private static final String PREFS_NAME = "BoardGameCrapPrefs";
+    public static final String[] EVENT_PROJECTION = new String[] {
+            CalendarContract.Events._ID,                           // 0
+            CalendarContract.Events.TITLE,                  // 1
+            CalendarContract.Events.DTSTART,         // 2
+            CalendarContract.Events.DTEND,                  // 3
+            CalendarContract.Events.HAS_ATTENDEE_DATA,
+            CalendarContract.Events.RRULE
+    };
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +38,42 @@ public class Events extends Activity {
 
         String calendarId = settings.getString("calendarId", "");
         Toast.makeText(getBaseContext(), "found calendarId: " + calendarId, Toast.LENGTH_LONG).show();
+
+        Cursor cursor = getEvents(calendarId);
+        String[] names = cursor.getColumnNames();
+        for (String name: names) {
+            Log.d("BOARDGAMECRAP", "column name " + name);
+        }
+        Log.d("BOARDGAMECRAP", "found " + cursor.getCount() + " events");
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getBaseContext(), android.R.layout.simple_list_item_1, getEvents(calendarId), new String[] {CalendarContract.Events.TITLE, }, new int[] {android.R.id.text1}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        final ListView listView = (ListView)findViewById(R.id.eventList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor item = (Cursor)listView.getItemAtPosition(position);
+
+                Toast.makeText(getBaseContext(), "chose " + item.getString(1) + " which starts at " + item.getString(2), Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 
     @SuppressWarnings("UnusedDeclaration")
     public void addEvent(View view) {
         Toast.makeText(getBaseContext(), "Not supported yet", Toast.LENGTH_LONG).show();
+    }
+
+
+    private Cursor getEvents(String calendarId) {
+
+        ContentResolver cr = getContentResolver();
+        Uri uri = CalendarContract.Events
+                .CONTENT_URI;
+        String selection = "(" + CalendarContract.Events.CALENDAR_ID + " = ?)";
+        String[] selectionArgs = new String[] {calendarId};
+        // Submit the query and get a Cursor object back.
+        return cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+
     }
 
 
